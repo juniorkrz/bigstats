47efb1fb59838276a7018cd21c37fe1a4b2c27b5
Krz <juniorkrz77@gmail.com>
2025-01-19 13:06:45 -0300

    format

--- src/app/config.php
+++ src/app/config.php
@@ -1,13 +1,15 @@
 <?php
 
-if(!defined('DIR'))
-	define('DIR',realpath(dirname(__FILE__)));
+if (!defined('DIR')) {
+	define('DIR', realpath(dirname(__FILE__)));
+}
 
-if(!defined('DIR_ROOT'))
+if (!defined('DIR_ROOT')) {
 	define('DIR_ROOT', str_replace(DIRECTORY_SEPARATOR, '/', dirname(DIR)));
+}
 
-require_once DIR."/../vendor/autoload.php";
-require_once DIR."/util/functions.php";
+require_once DIR . "/../vendor/autoload.php";
+require_once DIR . "/util/functions.php";
 
 // Configuração do timezone
 date_default_timezone_set('America/Recife');
